129ec139b843acd33219c8726cb20d5fad974353
Kbot64 <ktsky515@gmail.com>
2026-04-23 23:35:14 -0500

    Made a mistake and here are the proper function additions.

--- Kyle-Go/task_queue.go
+++ Kyle-Go/task_queue.go
@@ -1,9 +1,33 @@
 package main
 
-import "fmt"
-
 func main() {
-	
+
 }
 
-func scheduler()
+type task struct {
+	taskName     string
+	taskPriority int
+	taskLength   int
+	index        int
+}
+
+type PriorityQueue []*task
+
+func newTask(name string, priority int, length int) *task {
+	tName := task{taskName: name, taskPriority: priority, taskLength: length}
+	return &tName
+}
+
+func (pq PriorityQueue) Len() int {
+	return len(pq)
+}
+
+func (pq PriorityQueue) Less(index1, index2 int) bool {
+	return pq[index1].taskPriority < pq[index2].taskPriority
+}
+
+func (pq PriorityQueue) Swap(index1, index2 int) {
+	pq[index1], pq[index2] = pq[index2], pq[index1]
+	pq[index1].index = index1
+	pq[index2].index = index2
+}

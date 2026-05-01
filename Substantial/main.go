620cf293facab0b712b0e619968cbe7b6645891c
Colton Stanek <stanekcolton@gmail.com>
2026-04-30 22:04:27 -0500

    bst implementation

--- Substantial/main.go
+++ Substantial/main.go
@@ -27,17 +27,8 @@ func newNode(newData int) *Node {
 //-------------+-------------//
 
 type BST struct { // do we want to make it w generics?
-	root *Node
-
-	NewTree      func() *BST
-	Insert       func(*BST, int, int)
-	insertHelper func(*BST, *Node, *Node) (*Node, error)
-	Remove       func(*BST, int)
-	Get          func(*BST, int) int
-	size         func(*BST) int
-	height       func(*BST) int
-	max          func(*BST) *Node
-	min          func(*BST) *Node
+	root     *Node
+	treeSize int
 }
 
 func NewTree() *BST {
@@ -46,47 +37,50 @@ func NewTree() *BST {
 	}
 }
 
-func Insert(tree *BST, data int) {
+func (tree *BST) Insert(data int) {
 	var node *Node = newNode(data)
-	tree.root, _ = insertHelper(tree, nil, node)
+	tree.root, _ = tree.insertHelper(nil, node)
 }
 
-func insertHelper(tree *BST, currNode *Node, newNode *Node) (*Node, error) {
+func (tree *BST) insertHelper(currNode *Node, newNode *Node) (*Node, error) {
 	// if the root of the tree is nil, set the tree to the new node
 	if tree.root == nil {
 		tree.root = newNode
+		tree.treeSize++
 		return tree.root, nil
 	}
 	// if the current node is nil, then set it there
 	if currNode == nil {
 		currNode = newNode
+		tree.treeSize++
 		return currNode, nil
 	} else {
 		//tree's root is not null so continue down the tree
 		if newNode.data >= currNode.data {
 			// go to the right
-			insertHelper(tree, currNode.right, newNode)
+			tree.insertHelper(currNode.right, newNode)
 		} else if newNode.data < currNode.data {
 			// go to the left
-			insertHelper(tree, currNode.left, newNode)
+			tree.insertHelper(currNode.left, newNode)
 		}
 	}
 	return newNode, errors.New("Insertion failed, could not find proper placement.")
 }
 
-func Remove(tree *BST, data int) (int, error) {
+func (tree *BST) Remove(data int) (int, error) {
 	// call remove helper
-	newRoot, err := removeHelper(tree, tree.root, data)
+	newRoot, err := tree.removeHelper(tree.root, data)
 	// if the removehelper errors out, fail
 	if err != nil {
 		return 0, err
 	}
 	// if it did not error out, return the removed data
 	tree.root = newRoot
+	tree.treeSize--
 	return data, nil
 }
 
-func removeHelper(tree *BST, node *Node, dataToRemove int) (*Node, error) {
+func (tree *BST) removeHelper(node *Node, dataToRemove int) (*Node, error) {
 	// cannot find the value to be removed
 	if node == nil {
 		return nil, errors.New("Value not found in tree.")
@@ -95,12 +89,12 @@ func removeHelper(tree *BST, node *Node, dataToRemove int) (*Node, error) {
 	// searching for the piece of data to be removed
 	var err error
 	if dataToRemove < node.data {
-		node.left, err = removeHelper(tree, node.left, dataToRemove)
+		node.left, err = tree.removeHelper(node.left, dataToRemove)
 		if err != nil {
 			return nil, err
 		}
 	} else if node.data < dataToRemove {
-		node.right, err = removeHelper(tree, node.right, dataToRemove)
+		node.right, err = tree.removeHelper(node.right, dataToRemove)
 		if err != nil {
 			return nil, err
 		}
@@ -119,7 +113,7 @@ func removeHelper(tree *BST, node *Node, dataToRemove int) (*Node, error) {
 			curr = curr.left
 		}
 		node.data = curr.data
-		node.right, err = removeHelper(tree, node.right, curr.data)
+		node.right, err = tree.removeHelper(node.right, curr.data)
 		if err != nil {
 			return nil, err
 		}
@@ -127,7 +121,7 @@ func removeHelper(tree *BST, node *Node, dataToRemove int) (*Node, error) {
 	return node, nil
 }
 
-func Get(tree *BST, targetData int) (*Node, error) {
+func (tree *BST) Get(targetData int) (*Node, error) {
 	if tree == nil {
 		return nil, errors.New("Tree does not exist. ")
 	}
@@ -148,32 +142,78 @@ func Get(tree *BST, targetData int) (*Node, error) {
 	return nil, errors.New("Could not find node with that data")
 }
 
-func hasKey(tree *BST, key int) bool {
+func (tree *BST) HasKey(targetData int) bool {
+	// traverse through three until key is found, if key is not found then return false.
+	var currNode *Node = tree.root
+	for currNode != nil {
+		if currNode.data == targetData {
+			return true
+		}
+		if targetData < currNode.data {
+			// go to the left
+			currNode = currNode.left
+		} else if targetData > currNode.data {
+			currNode = currNode.right
+		}
+	}
 	return false
 }
 
-func size(tree *BST) int {
-	return 0
+func (tree *BST) Size() int {
+	return tree.treeSize
 }
 
-func height(tree *BST) int {
-	return 0
+func (tree *BST) Height() int {
+	return tree.height(tree.root)
 }
 
-func maxKey(tree *BST) int {
-	return 0
+func (tree *BST) height(node *Node) int {
+	if node == nil {
+		return -1
+	}
+
+	var l int = tree.height(node.left)
+	var r int = tree.height(node.right)
+
+	if l > r {
+		return 1 + l
+	} else {
+		return 1 + r
+	}
 }
 
-func minKey(tree *BST) int {
-	return 0
+func (tree *BST) MaxKey() int {
+	return tree.Max().data
 }
 
-func max(tree *BST) *Node {
-	return nil
+func (tree *BST) MinKey() int {
+	return tree.Min().data
 }
 
-func min(tree *BST) *Node {
-	return nil
+func (tree *BST) Max() *Node {
+	if tree.root == nil {
+		return nil
+	}
+
+	var currNode *Node = tree.root
+	for currNode.right != nil {
+		currNode = currNode.right
+	}
+
+	return currNode
+}
+
+func (tree *BST) Min() *Node {
+	if tree.root == nil {
+		return nil
+	}
+
+	var currNode *Node = tree.root
+	for currNode.left != nil {
+		currNode = currNode.left
+	}
+
+	return currNode
 }
 
 func main() {

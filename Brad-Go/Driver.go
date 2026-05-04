a0812a42c80d41bc6f87cbd4885289a98d5e9857
Brad Wickert <wbradwickert@gmail.com>
2026-05-04 10:31:11 -0500

    Updated program to use more concurrency
    Refactor Driver and Hotel structs to use concurrency for data generation and display. Improve performance by using goroutines and WaitGroups.

--- Brad-Go/Driver.go
+++ Brad-Go/Driver.go
@@ -3,82 +3,162 @@ package main
 import (
 	"fmt"
 	"math/rand/v2"
-	"os"
 	"strconv"
 	"strings"
-
-	//"sync"
+	"sync"
 	"time"
 )
 
 type Driver struct {
-	hotelList []*Hotel
+	hotelSlice []*Hotel
 }
 
+/*
+GenerateData method that concurrentyly makes Hotel structs and puts them in the hotel slice.
+*/
 func (driver *Driver) GenerateData() {
-	//Preallocate slice
-	driver.hotelList = make([]*Hotel, 0, 8)
+	//Slice literal of strings of hotel names.
+	names := []string{
+		"Ritz Carlton, Chicago\t",
+		"Four Seasons, Chicago\t",
+		"Beijing Hotel\t\t",
+		"JW Marriot, Beijing\t",
+		"Mandarin Oriental, Barcelona",
+		"The Peninsula, Chicago\t",
+		"Shangri La, Beijing\t",
+		"Waldorf Astoria, Chicago",
+	}
 
-	driver.hotelList = append(driver.hotelList, NewHotel("Ritz Carlton, Chicago\t"))
-	driver.hotelList = append(driver.hotelList, NewHotel("Four Seasons, Chicago\t"))
-	driver.hotelList = append(driver.hotelList, NewHotel("Beijing Hotel\t\t"))
-	driver.hotelList = append(driver.hotelList, NewHotel("JW Marriot, Beijing\t"))
-	driver.hotelList = append(driver.hotelList, NewHotel("Mandarin Oriental, Barcelona"))
-	driver.hotelList = append(driver.hotelList, NewHotel("The Peninsula, Chicago\t"))
-	driver.hotelList = append(driver.hotelList, NewHotel("Shangri La, Beijing\t"))
-	driver.hotelList = append(driver.hotelList, NewHotel("Waldorf Astoria, Chicago"))
+	//Allocate the slice of hotel pointers that are nil pointers for now, so the goroutine can write them in.
+	driver.hotelSlice = make([]*Hotel, len(names))
+
+	//Declare a WaitGroup, which acts as a boss/coordinator for the goroutines.
+	var wg sync.WaitGroup
+
+	//Iterate over the name slice.
+	for i, name := range names {
+
+		//Increment WaitGroup by 1 before starting goroutineto tell wg.Wait() that a goroutine isn't finished yet.
+		wg.Add(1)
+		go func(index int, hotelName string) {
+			//Decrememnt WaitGroup once goruotine is finsihed to tell wg.wait() that the goroutine is finished.
+			defer wg.Done()
+
+			//Create a new Hotel and insert it into the slice index.
+			driver.hotelSlice[index] = NewHotel(hotelName)
+		}(i, name)
+	}
+
+	//GenerateData method wont be able to stop until WaitGroup reaches 0.
+	wg.Wait()
+
 }
 
+/*
+DisplayBanner method that prints the year banner and divider.
+*/
 func DisplayBanner() {
 	fmt.Println("Hotel\t\t\t\t2025\t2024\t2023\t2022\t2021\t2020\t2019\t2018\t2017\t2016")
 	fmt.Println("----------------------------------------------------------------------------------------------------------------")
 }
 
+/*
+DisplayAllRatings method that concurrently builds fomratted hotel rating strings and prints them.
+*/
 func (driver *Driver) DisplayAllRatings() {
+
 	fmt.Println("All ratings:")
 	DisplayBanner()
 
-	for _, hotel := range driver.hotelList {
-		fmt.Println(hotel.ToString())
+	//Allocate the string slice of hotel info.
+	results := make([]string, len(driver.hotelSlice))
+
+	//Declare a WaitGroup, which acts as a boss/coordinator for the goroutines.
+	var wg sync.WaitGroup
+
+	for i, hotel := range driver.hotelSlice {
+
+		//Incrememnt WaitGroup.
+		wg.Add(1)
+		//Both index and h are passed here so goroutines dont share the same i and hotel values.
+		go func(index int, h *Hotel) {
+			defer wg.Done()
+			results[index] = h.ToString()
+		}(i, hotel)
+	}
+	//Dont advance until all goroutines are finsihed.
+	wg.Wait()
+
+	for _, line := range results {
+		fmt.Println(line)
 	}
 }
 
+/*
+FindBestRating concurrent function that finds the hotels with the best rating using channels.
+*/
 func (driver *Driver) FindBestRating() {
 
-	highRating := make([]bool, 8)
-	highRatingFound := false
-	fmt.Println("\nThe Best Hotels:")
-	for i, hotel := range driver.hotelList {
-		for _, rating := range hotel.ratings {
-			if rating >= 95 {
-				highRating[i] = true
-				highRatingFound = true
+	//Make a channel of ints with a slot for each hotel.
+	results := make(chan int, len(driver.hotelSlice))
+
+	var wg sync.WaitGroup
+
+	for i, hotel := range driver.hotelSlice {
+		wg.Add(1)
+		go func(index int, h *Hotel) {
+			defer wg.Done()
+			for _, rating := range h.ratings {
+
+				//Send hotel's index into the channel and return it.
+				if rating >= 95 {
+					results <- index
+					return
+				}
 			}
-		}
-
+		}(i, hotel)
 	}
 
-	if highRatingFound {
-		DisplayBanner()
-	} else {
+	//Launch goroutine that waits for other goroutines to finish.
+	//(Its a goroutine so the main flow is not blocked)
+	go func() {
+		wg.Wait()
+		//Close the channels and stop blocking.
+		close(results)
+	}()
+
+	highRating := make([]bool, len(driver.hotelSlice))
+	highRatingFound := false
+	for index := range results {
+		highRating[index] = true
+		highRatingFound = true
+	}
+
+	fmt.Println("\nThe Best Hotels:")
+	if !highRatingFound {
 		fmt.Println("Sorry, there's no hotels that recieved a 95 or above rating.")
+		return
 	}
 
-	for index := range driver.hotelList {
-		if highRating[index] == true {
-			fmt.Println(driver.hotelList[index].ToString())
+	DisplayBanner()
+	for index, isHigh := range highRating {
+		if isHigh {
+			fmt.Println(driver.hotelSlice[index].ToString())
 		}
 	}
 
 }
 
+/*
+FindWorstRating method that finds the worst rated hotel sequentially.
+*/
 func (driver *Driver) FindWorstRating() {
 
 	//Set to 100 because a rating of 100 should be checked aswell.
 	lowestRating := 101
 	hotelLowest := 0
 	numLowRatings := 0
-	for i, hotel := range driver.hotelList {
+	for i, hotel := range driver.hotelSlice {
 		//Dont need the index of rating, just the rating value.
 		for _, rating := range hotel.ratings {
 			//Skip a rating of 0 since the hotel was not open/operating during that time.
@@ -98,7 +178,7 @@ func (driver *Driver) FindWorstRating() {
 
 	fmt.Printf("\nThe Worst Hotel:\nThe following hotel has recieved the worst rating of %v, %v time(s).\n", lowestRating, numLowRatings)
 	DisplayBanner()
-	fmt.Println(driver.hotelList[hotelLowest].ToString())
+	fmt.Println(driver.hotelSlice[hotelLowest].ToString())
 
 }
 
@@ -107,22 +187,39 @@ type Hotel struct {
 	ratings []int
 }
 
+/*
+NewHotel method that makes and returns a new Hotel struct with random ratings slice.
+*/
 func NewHotel(name string) *Hotel {
+
+	//Allocate a int slice for 10 years.
 	ratingsTemp := make([]int, 10)
+
+	//rand.IntN(6)+5 -> Random number between 5 and 10.
+	//This makes it so the most years a hotel can be unavailible for early on is 4 years, which keeps rating at 0. (N/A rating)
 	for i := 0; i < rand.IntN(6)+5; i++ {
 		ratingsTemp[i] = rand.IntN(100) + 1
 	}
+
+	//Make and return the pointer to the hotel struct with the name and new ratings slice.
+	//Returning a pointer so whoever calls NewHotel can reference and modify the same Hotel in memory.
 	return &Hotel{name: name, ratings: ratingsTemp}
 }
 
+/*
+ToString method that builds a string for a hotels name and ratings sequentially.
+*/
 func (hotel *Hotel) ToString() string {
 	var strBuilder strings.Builder
+
+	//Pre-allocates space for the string builder since we know how long the string will be.
 	strBuilder.Grow(64)
 	strBuilder.WriteString(hotel.name + "\t")
 	for index := range hotel.ratings {
 		if hotel.ratings[index] == 0 {
 			strBuilder.WriteString("N/A\t")
 		} else {
+			//Convert the int rating to a string using strconv.Itoa.
 			strBuilder.WriteString(strconv.Itoa(hotel.ratings[index]) + "\t")
 		}
 
@@ -131,14 +228,15 @@ func (hotel *Hotel) ToString() string {
 }
 
 func main() {
+	//Start recording time.
 	start := time.Now()
+
+	//Define an anonomuys function that executes when main returns.
+	defer func() { fmt.Printf("Program took %s to run. \n", time.Since(start)) }()
+
 	var driver Driver
 	driver.GenerateData()
 	driver.DisplayAllRatings()
 	driver.FindBestRating()
 	driver.FindWorstRating()
-	elapsed := time.Since(start)
-	fmt.Printf("Program took %s to run.\n", elapsed)
-	os.Exit(3)
-
 }
